cc616f6f7730a212f74b4178841563db12c59c4a
honey <594107385@qq.com>
2020-12-17 15:29:49 +0800

    author

---
+++ universe-core/src/main/java/wjy/com/universe/DeMo.java
@@ -0,0 +1,13 @@
+package wjy.com.universe;
+
+import java.util.ArrayList;
+
+public class DeMo {
+    private static final ArrayList<String> author = new ArrayList<>();
+    static{
+        author.add("author: wangjunyou");
+    }
+    public static void main(String[] args) {
+        author.forEach(System.out::println);
+    }
+}
